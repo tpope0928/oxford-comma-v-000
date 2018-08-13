8183e26e552a2903501cd6df87ba39cab3c47bbd
Thomas Pope <tpope0928@gmail.com>
2018-08-13 22:36:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,6 @@ def oxford_comma(array)
   case array.length
     when 1
       "#{array[0]"
-    when 2 
-      array[0...2].join (" and ")
-    
-end
+    when 2
+      array[0..1].join(" and ")
+    when 
